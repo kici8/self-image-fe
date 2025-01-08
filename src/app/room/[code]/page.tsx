9d2167cce6a6b38c4058b0ffa8df26157e23f268
Kici8 <artusi.daniele@gmail.com>
2025-01-08 16:39:22 +0100

    chore: Move the room jsx out of the route

--- src/app/room/[code]/page.tsx
+++ src/app/room/[code]/page.tsx
@@ -1,9 +1,6 @@
-import { Button } from "@/components/ui/button";
-import { DownloadIcon, FileX2Icon, RefreshCwIcon } from "lucide-react";
-import CategoryList from "./components/CategoryList";
-import ImageGrid from "./components/ImageGrid";
+import Alert from "@/components/Alert";
+import Room from "./components/Room";
 
-// TODO: maybe use query params instead of path params and make the page client-side only
 export default async function Page({
   params,
 }: {
@@ -11,78 +8,11 @@ export default async function Page({
 }) {
   const code = (await params).code;
 
-  // TODO: listen for changes in the room and update the UI accordingly
-  // On the main content show the gallery of images
-
-  // On the sidebar show:
-
-  // // The room code ✅
-  // // The qr to join the room // TODO: ask what data to put in the qr
-  // // The list of users that ask to join the room or just the counter
-
-  // // Results and categories ✅
-
-  // // New session button ✅
-  // // Download results button ✅
-  // // Close room button ✅
-
-  return (
-    <div className="flex h-svh">
-      <div className="flex-1">
-        <ImageGrid />
+  if (code) return <Room code={code} />;
+  else
+    return (
+      <div className="mx-auto mt-16 max-w-md">
+        <Alert type="error" title="Errore: impossibile trovare la stanza" />
       </div>
-
-      <div className="flex min-w-96 flex-grow-0 flex-col gap-6 p-3">
-        <div className="flex flex-grow-0 flex-col gap-6 rounded-xl bg-card p-3">
-          <div className="flex">
-            <div className="flex h-24 w-24 items-center justify-center rounded-sm bg-white p-1">
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                src={`https://api.qrserver.com/v1/create-qr-code/?size=256x256&data=${code}`}
-                alt={`QR code for room ${code}`}
-                className="block flex-1"
-              />
-            </div>
-
-            <div className="flex flex-1 flex-col justify-center px-6">
-              <h2 className="text-4xl font-semibold italic">{code}</h2>
-              <p className="text-sm text-muted-foreground">
-                Entra nella stanza
-              </p>
-            </div>
-          </div>
-
-          <div className="flex flex-col text-sm">
-            <div className="flex gap-1 text-muted-foreground">
-              Utenti entrati:
-              <span className="font-bold text-primary">21</span>
-            </div>
-            <div className="flex gap-1 text-muted-foreground">
-              Record inviati in questa sessione:
-              <span className="font-bold text-primary">3</span>
-            </div>
-          </div>
-          <Button>
-            Nuova sessione
-            <RefreshCwIcon />
-          </Button>
-        </div>
-
-        <div className="flex flex-1 flex-col gap-2">
-          <CategoryList />
-        </div>
-
-        <div className="flex gap-2 rounded-lg bg-card px-4 py-4">
-          <Button variant="outline" className="flex-1">
-            Scarica risultati
-            <DownloadIcon />
-          </Button>
-          <Button variant="destructive" className="flex-1">
-            Chiudi stanza
-            <FileX2Icon />
-          </Button>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,86 +1,86 @@
-import { cn } from '@/lib/utils';
-import { cva } from 'class-variance-authority';
+import { cn } from "@/lib/utils";
+import { cva } from "class-variance-authority";
 import {
   CircleCheckIcon,
   CircleXIcon,
   InfoIcon,
   TriangleAlertIcon,
-} from 'lucide-react';
-import { JSX } from 'react';
+} from "lucide-react";
+import { JSX } from "react";
 
-type AlertType = 'info' | 'warning' | 'error' | 'success';
+type AlertType = "info" | "warning" | "error" | "success";
 
 type AlertProps = {
   type: AlertType;
   title: string;
-  messages: string[];
+  messages?: string[];
 };
 
 export default function Alert({
-  type = 'warning',
+  type = "warning",
   title,
   messages,
 }: AlertProps) {
-  const containerVariants = cva('rounded-md bg-opacity-10 p-4', {
+  const containerVariants = cva("rounded-md bg-opacity-10 p-4", {
     variants: {
       type: {
-        info: 'bg-blue-500',
-        warning: 'bg-yellow-500',
-        error: 'bg-red-500',
-        success: 'bg-green-500',
+        info: "bg-blue-500",
+        warning: "bg-yellow-500",
+        error: "bg-red-500",
+        success: "bg-green-500",
       },
     },
   });
 
-  const iconVariants = cva('size-5', {
+  const iconVariants = cva("size-5", {
     variants: {
       type: {
-        info: 'text-blue-700 dark:text-blue-300',
-        warning: 'text-yellow-700 dark:text-yellow-300',
-        error: 'text-red-700 dark:text-red-300',
-        success: 'text-green-700 dark:text-green-300',
+        info: "text-blue-700 dark:text-blue-300",
+        warning: "text-yellow-700 dark:text-yellow-300",
+        error: "text-red-700 dark:text-red-300",
+        success: "text-green-700 dark:text-green-300",
       },
     },
   });
 
-  const titleVariants = cva('font-semibold', {
+  const titleVariants = cva("font-semibold", {
     variants: {
       type: {
-        info: 'text-blue-900 dark:text-blue-100',
-        warning: 'text-yellow-900 dark:text-yellow-100',
-        error: 'text-red-900 dark:text-red-100',
-        success: 'text-green-900 dark:text-green-100',
+        info: "text-blue-900 dark:text-blue-100",
+        warning: "text-yellow-900 dark:text-yellow-100",
+        error: "text-red-900 dark:text-red-100",
+        success: "text-green-900 dark:text-green-100",
       },
     },
   });
 
-  const messageVariants = cva('mt-2 text-sm', {
+  const messageVariants = cva("mt-2 text-sm", {
     variants: {
       type: {
-        info: 'text-blue-700 dark:text-blue-300',
-        warning: 'text-yellow-700 dark:text-yellow-300',
-        error: 'text-red-700 dark:text-red-300',
-        success: 'text-green-700 dark:text-green-300',
+        info: "text-blue-700 dark:text-blue-300",
+        warning: "text-yellow-700 dark:text-yellow-300",
+        error: "text-red-700 dark:text-red-300",
+        success: "text-green-700 dark:text-green-300",
       },
     },
   });
 
   const ICON_MAP: Record<AlertType, JSX.Element> = {
     info: (
-      <InfoIcon aria-hidden='true' className={cn(iconVariants({ type }))} />
+      <InfoIcon aria-hidden="true" className={cn(iconVariants({ type }))} />
     ),
     warning: (
       <TriangleAlertIcon
-        aria-hidden='true'
+        aria-hidden="true"
         className={cn(iconVariants({ type }))}
       />
     ),
     error: (
-      <CircleXIcon aria-hidden='true' className={cn(iconVariants({ type }))} />
+      <CircleXIcon aria-hidden="true" className={cn(iconVariants({ type }))} />
     ),
     success: (
       <CircleCheckIcon
-        aria-hidden='true'
+        aria-hidden="true"
         className={cn(iconVariants({ type }))}
       />
     ),
@@ -88,15 +88,15 @@ export default function Alert({
 
   return (
     <div className={cn(containerVariants({ type }))}>
-      <div className='flex'>
-        <div className='shrink-0'>{ICON_MAP[type]}</div>
-        <div className='ml-3'>
+      <div className="flex">
+        <div className="shrink-0">{ICON_MAP[type]}</div>
+        <div className="ml-3">
           <h3 className={cn(titleVariants({ type }))}>{title}</h3>
 
-          {messages.length ? (
+          {messages && messages.length ? (
             <div className={cn(messageVariants({ type }))}>
               {messages.length > 1 ? (
-                <ul role='list' className='list-disc space-y-1 pl-5'>
+                <ul role="list" className="list-disc space-y-1 pl-5">
                   {messages.map((message) => (
                     <li key={message}>{message}</li>
                   ))}

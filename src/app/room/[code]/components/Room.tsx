ba5bc3b169c672515d5410b1510ff2ba609f4321
Kici8 <artusi.daniele@gmail.com>
2025-01-10 17:19:53 +0100

    chore: move hooks inside the lib folder

--- src/app/room/[code]/components/Room.tsx
+++ src/app/room/[code]/components/Room.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { DownloadIcon, FileX2Icon, RefreshCwIcon } from "lucide-react";
 import ImageGrid from "./ImageGrid";
 import CategoryList from "./CategoryList";
-import { RoomUpdatedResponse, useSocket } from "@/app/hooks/useSocket";
+import { RoomUpdatedResponse, useSocket } from "@/lib/hooks/useSocket";
 import { useEffect, useState } from "react";
 
 export default function Room({ code }: { code: string }) {

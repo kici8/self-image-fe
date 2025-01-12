0e986d4e6096a86baadde7834969af2546b2ca9e
Kici8 <artusi.daniele@gmail.com>
2025-01-12 20:51:45 +0100

    fix: lock icon width

--- src/app/room/[code]/components/ImageGrid.tsx
+++ src/app/room/[code]/components/ImageGrid.tsx
@@ -44,7 +44,7 @@ export default function ImageGrid({ images }: { images: TypeImage[] }) {
                     clipRule="evenodd"
                     strokeLinejoin="round"
                     strokeMiterlimit="2"
-                    className="h-16 w-16"
+                    className="w-[40%]"
                     fill="currentColor"
                   >
                     <path d="M18,9L19,9C20.656,9 22,10.344 22,12L22,20C22,21.656 20.656,23 19,23L5,23C3.344,23 2,21.656 2,20L2,12C2,10.344 3.344,9 5,9L6,9L6,7C6,3.708 8.708,1 12,1C15.292,1 18,3.708 18,7L18,9ZM16,9L16,7C16,4.806 14.194,3 12,3C9.806,3 8,4.806 8,7L8,9L16,9ZM12,14C10.896,14 10,14.896 10,16C10,17.104 10.896,18 12,18C13.104,18 14,17.104 14,16C14,14.896 13.104,14 12,14Z" />

d7b47fc0ff74db85d9d5e3c79012ff564335df87
Kici8 <artusi.daniele@gmail.com>
2025-01-28 10:13:11 +0100

    feat: show unlocked images button

--- src/app/room/[code]/components/ImageGrid.tsx
+++ src/app/room/[code]/components/ImageGrid.tsx
@@ -1,25 +1,27 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
-import { cva } from "class-variance-authority";
-import {
-  CameraIcon,
-  CameraOffIcon,
-  MoonIcon,
-  SunIcon,
-  ZoomInIcon,
-  ZoomOutIcon,
-} from "lucide-react";
-import Image from "next/image";
-import { useState } from "react";
-import { TypeImage } from "../../../../lib/referenceElements";
-import ImageDialog from "./ImageDialog";
-import { useTheme } from "next-themes";
 import {
   Tooltip,
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { cn } from "@/lib/utils";
+import { cva } from "class-variance-authority";
+import {
+  CameraIcon,
+  CameraOffIcon,
+  LockKeyholeIcon,
+  LockKeyholeOpenIcon,
+  MoonIcon,
+  SunIcon,
+  ZoomInIcon,
+  ZoomOutIcon,
+} from "lucide-react";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import { useState } from "react";
+import { TypeImage } from "../../../../lib/referenceElements";
+import ImageDialog from "./ImageDialog";
 
 export enum typeGridType {
   image = "IMAGE",
@@ -38,8 +40,9 @@ export default function ImageGrid({ images }: { images: TypeGridImage[] }) {
   const { setTheme, theme } = useTheme();
 
   // States
-  const [numberOfColumns, setNumberOfColumns] = useState(5);
+  const [numberOfColumns, setNumberOfColumns] = useState(9);
   const [showSelfie, setShowSelfie] = useState(true);
+  const [showLockedImages, setShowLockedImages] = useState(false);
   const [imageModalOpen, setImageModalOpen] = useState<TypeGridImage | null>(
     null,
   );
@@ -135,43 +138,67 @@ export default function ImageGrid({ images }: { images: TypeGridImage[] }) {
             {showSelfie ? <p>Nascondi selfie</p> : <p>Mostra selfie</p>}
           </TooltipContent>
         </Tooltip>
+
+        <Tooltip delayDuration={125}>
+          <TooltipTrigger asChild>
+            <Button
+              className={cn(
+                ToggleButtonVariants({ checked: showLockedImages }),
+              )}
+              size="icon"
+              onClick={() => setShowLockedImages((prev) => !prev)}
+            >
+              {showLockedImages ? <LockKeyholeOpenIcon /> : <LockKeyholeIcon />}
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent side="bottom" sideOffset={8}>
+            {showLockedImages ? (
+              <p>Nascondi bloccate</p>
+            ) : (
+              <p>Mostra bloccate</p>
+            )}
+          </TooltipContent>
+        </Tooltip>
       </div>
 
       <div className="gap-1" style={{ columns: numberOfColumns }}>
-        {filteredImages.map((image) => (
-          <div
-            key={image.id}
-            className="mb-1"
-            onClick={() => setImageModalOpen(image)}
-          >
-            <div className="relative overflow-hidden">
-              <Image
-                src={image.src}
-                // TODO: pass the right image width and height
-                // to avoid layout shift
-                width={720}
-                height={720}
-                alt=""
-                className={cn(imageVariants({ unlocked: image.unlocked }))}
-              />
-              {!image.unlocked ? (
-                <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center bg-card text-card-foreground/10">
-                  <svg
-                    viewBox="0 0 24 24"
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    strokeLinejoin="round"
-                    strokeMiterlimit="2"
-                    className="w-[25%]"
-                    fill="currentColor"
-                  >
-                    <path d="M18,9L19,9C20.656,9 22,10.344 22,12L22,20C22,21.656 20.656,23 19,23L5,23C3.344,23 2,21.656 2,20L2,12C2,10.344 3.344,9 5,9L6,9L6,7C6,3.708 8.708,1 12,1C15.292,1 18,3.708 18,7L18,9ZM16,9L16,7C16,4.806 14.194,3 12,3C9.806,3 8,4.806 8,7L8,9L16,9ZM12,14C10.896,14 10,14.896 10,16C10,17.104 10.896,18 12,18C13.104,18 14,17.104 14,16C14,14.896 13.104,14 12,14Z" />
-                  </svg>
-                </div>
-              ) : null}
+        {filteredImages.map((image) => {
+          const isImageUnlocked = image.unlocked || showLockedImages;
+          return (
+            <div
+              key={image.id}
+              className="mb-1"
+              onClick={() => setImageModalOpen(image)}
+            >
+              <div className="relative overflow-hidden">
+                <Image
+                  src={image.src}
+                  // TODO: pass the right image width and height
+                  // to avoid layout shift
+                  width={720}
+                  height={720}
+                  alt=""
+                  className={cn(imageVariants({ unlocked: isImageUnlocked }))}
+                />
+                {!isImageUnlocked ? (
+                  <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center bg-card text-card-foreground/10">
+                    <svg
+                      viewBox="0 0 24 24"
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      strokeLinejoin="round"
+                      strokeMiterlimit="2"
+                      className="w-[25%]"
+                      fill="currentColor"
+                    >
+                      <path d="M18,9L19,9C20.656,9 22,10.344 22,12L22,20C22,21.656 20.656,23 19,23L5,23C3.344,23 2,21.656 2,20L2,12C2,10.344 3.344,9 5,9L6,9L6,7C6,3.708 8.708,1 12,1C15.292,1 18,3.708 18,7L18,9ZM16,9L16,7C16,4.806 14.194,3 12,3C9.806,3 8,4.806 8,7L8,9L16,9ZM12,14C10.896,14 10,14.896 10,16C10,17.104 10.896,18 12,18C13.104,18 14,17.104 14,16C14,14.896 13.104,14 12,14Z" />
+                    </svg>
+                  </div>
+                ) : null}
+              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
 
       {imageModalOpen ? (

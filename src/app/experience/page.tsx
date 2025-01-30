2e524acf8e0ef48b62ce792bc55753e2d0dacfb1
Kici8 <artusi.daniele@gmail.com>
2025-01-30 17:27:48 +0100

    feat: filters test whit face_landmarker

---
+++ src/app/experience/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import FaceTracker from "./components/FaceTracker";
+
+const ExperiencePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Experience</h1>
+      <FaceTracker />
+    </div>
+  );
+};
+
+export default ExperiencePage;

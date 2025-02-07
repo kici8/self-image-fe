6c534b838fe29daac5aea4262f59cd959acd5536
Kici8 <artusi.daniele@gmail.com>
2025-02-07 14:36:49 +0100

    fix: test safari screenshot export with custom shader

--- src/app/experience/filters/XRayFilter.tsx
+++ src/app/experience/filters/XRayFilter.tsx
@@ -1,42 +1,57 @@
 "use client";
 import { FaceLandmarkerResult } from "@mediapipe/tasks-vision";
 import { useLoader } from "@react-three/fiber";
+import { EffectComposer } from "@react-three/postprocessing";
 import React from "react";
 import * as THREE from "three";
 import FaceMeshComponent from "../components/FaceMesh";
 
-// Component properties interface
+import { Effect } from "postprocessing";
+import { forwardRef, useMemo } from "react";
+
+const fragmentShader = `
+  uniform sampler2D tDiffuse;
+  vec4 mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 targetColor) {
+    targetColor = vec4(vec3(1.0 - inputColor.rgb), inputColor.a);
+    return targetColor;
+  }
+`;
+
+class InvertEffectImpl extends Effect {
+  constructor() {
+    super("InvertEffect", fragmentShader, {
+      uniforms: new Map(),
+    });
+  }
+}
+
+const InvertEffect = forwardRef((props, ref) => {
+  const effect = useMemo(() => new InvertEffectImpl(), []);
+  return <primitive ref={ref} object={effect} dispose={null} />;
+});
+InvertEffect.displayName = "InvertEffect";
+
 interface Props {
   faceLandmarkerResult: FaceLandmarkerResult;
   aspect: number;
 }
 
 const XRayFilter: React.FC<Props> = ({ faceLandmarkerResult, aspect }) => {
-  const faceMeshRef = React.useRef<THREE.Mesh | null>(null);
-  //  Load texture and prevent flipping on Y axis.
+  // Load texture for the FaceMesh component
   const texture = useLoader(THREE.TextureLoader, "/ar/uv-x-ray-small.jpg");
   texture.flipY = false;
 
   return (
-    // Render the mesh. The mesh will use the material that applies the texture.
     <>
       <FaceMeshComponent
         faceLandmarkerResult={faceLandmarkerResult}
         aspect={aspect}
         texture={texture}
         opacity={0.6}
-        ref={faceMeshRef}
       />
-      {/* Check if removing the EffectComposer will fix the issue with iOs safari screenshot not working */}
-      {/* <EffectComposer>
-        <BrightnessContrast
-          brightness={0.8}
-          contrast={-0.9}
-          opacity={0.99}
-          blendFunction={BlendFunction.INVERT_RGB}
-        />
-        <Sepia opacity={1} blendFunction={BlendFunction.NORMAL} />
-      </EffectComposer> */}
+      <EffectComposer>
+        <InvertEffect />
+      </EffectComposer>
     </>
   );
 };

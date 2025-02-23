c6d3420ae40ca6a28378a20d2cdef7c9aa31955a
Kici8 <artusi.daniele@gmail.com>
2025-02-23 14:55:57 +0100

    feat: add metadata

--- src/app/room/[code]/page.tsx
+++ src/app/room/[code]/page.tsx
@@ -1,11 +1,24 @@
 import Alert from "@/components/Alert";
+import { Metadata } from "next";
 import Room from "./components/Room";
 
-export default async function Page({
-  params,
-}: {
+type Props = {
   params: Promise<{ code: string }>;
-}) {
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  // read route params
+  const code = (await params).code;
+
+  return {
+    title: `Self Image - Stanza ${code}`,
+    openGraph: {
+      title: `Self Image - Stanza ${code}`,
+    },
+  };
+}
+
+export default async function Page({ params }: Props) {
   const code = (await params).code;
 
   if (code) return <Room code={code} />;
